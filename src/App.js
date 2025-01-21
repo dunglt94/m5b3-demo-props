1aaf0e13d205ca8df5e9d309df48067031db6e5b
dunglt94 <tuandung9594@gmail.com>
2025-01-21 11:20:33 +0700

    add AddComponent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import Welcome from './components/Welcome';
-function App() {
-  return (
-      <div className="App">
-        <Welcome name="Admin" />
-      </div>
-  );
-}
+// import Welcome from './components/Welcome';
+// function App() {
+//   return (
+//       <div className="App">
+//         <Welcome name="Admin" />
+//       </div>
+//   );
+// }
 
+import AddComponent from './components/AddComponent';
+function App() {
+    return (
+        <AddComponent firstNumber={1} secondNumber={2} />
+    );
+}
 export default App;
